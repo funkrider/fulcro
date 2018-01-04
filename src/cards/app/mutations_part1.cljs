a037b2caf74fe08794cb7b89e77ed97b84d3dad8
funkrider <ian_davies@hotmail.com>
2018-01-03 22:30:53 -0500

    Make card complete fulcro app

--- src/cards/app/mutations_part1.cljs
+++ src/cards/app/mutations_part1.cljs
@@ -5,17 +5,24 @@
                [fulcro.client.primitives :as prim :refer [defsc]]
                [app.ui.components :as comp]))
 
-  (defsc TodoItem [this {:keys [db/id item/label item/complete? ui/editing?] :as props}]
-         (dom/li nil
-           (dom/input #js {:type "checkbox"
-                           :checked complete?})
-           (if editing?
-             (dom/input #js {:type "text"
-                             :value label})
-             label)))
+(defsc TodoItem [this {:keys [db/id
+                              item/label
+                              item/complete?
+                              ui/editing?] :as props}]
+  {:initial-state {:db/id 1
+                   :item/label "Buy stuff"
+                   :item/complete? false
+                   :ui/editing? true}}
+       (dom/li nil
+         (dom/input #js {:type "checkbox"
+                         :checked complete?})
+         (if editing?
+           (dom/input #js {:type "text"
+                           :value label})
+           label)))
 
-  (def ui-todo-item (prim/factory TodoItem))
-
-  (defcard todo-item-unchecked
-           "An un-checked todo item"
-           (ui-todo-item {:db/id 1 :item/label "Buy Milk" :item/complete? false}))
+(defcard-fulcro active-todo-item-1
+  "An active todo item with mutations"
+  TodoItem
+  {} ;; empty initial db
+  {:inspect-data true})
