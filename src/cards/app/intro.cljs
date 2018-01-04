cc5a4552538ce8d4f9bb61303fa1d80498eaa421
funkrider <ian_davies@hotmail.com>
2018-01-03 21:10:47 -0500

    Change to use defcard-fulcro. Creates a stand alone app in a card.

--- src/cards/app/intro.cljs
+++ src/cards/app/intro.cljs
@@ -1,21 +1,16 @@
 (ns app.intro
   (:require [devcards.core :as rc :refer-macros [defcard]]
+            [fulcro.client.cards :refer [defcard-fulcro]]
             [fulcro.client.dom :as dom]
             [fulcro.client.primitives :as prim :refer [defsc]]
             [app.ui.components :as comp]))
 
-(defcard SVGPlaceholder
-  "# SVG Placeholder"
-  (comp/ui-placeholder {:w 200 :h 200}))
+(defsc Root [this {:keys [name] :as props}]
+  (js/console.log props)
+  (dom/div nil
+    (str "hi " name)))
 
-(defsc ThimgRenderer [this {:keys [name]}]
-  (dom/div #js {:className "boo"}
-    (dom/b nil
-      (str "hi " name))))
-
-(def thing-renderer (prim/factory ThimgRenderer))
-
-(defcard my-card
-  "# Stuff
-  This is markdown..."
-  (thing-renderer {:name "Ian"}))
+(defcard-fulcro my-card
+  Root
+  {} ;; Default data
+  {:inspect-data true :name "Ian"})
