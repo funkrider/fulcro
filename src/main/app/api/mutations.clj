c4588e56e9def02d19bd02000e6d3d1c7cdee4d1
funkrider <ian_davies@hotmail.com>
2018-01-08 12:09:04 -0500

    Add a mutation to the server side that accepts the named type now sent by the client. Have this mutation swap the in memory database for a new task name.

--- src/main/app/api/mutations.clj
+++ src/main/app/api/mutations.clj
@@ -1,7 +1,12 @@
 (ns app.api.mutations
   (:require
     [taoensso.timbre :as timbre]
+    [app.api.read :refer [todo-list-database]]
     [fulcro.server :refer [defmutation]]))
 
 ;; Place your server mutations here
 
+(defmutation app.server-interactions/change-item-label [{:keys [id text] :as params}]
+  (action [env]
+    (timbre/info "Change label " params)
+    (swap! todo-list-database assoc-in [:items id :item/label] text)))
